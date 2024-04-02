871256c96ec16dbeda6b6f48b2a0b3fc59869a57
amitark7 <kumaramit21470@gmal.com>
2024-04-02 11:11:04 +0530

    add meaning message to each function

--- index.js
+++ index.js
@@ -15,21 +15,29 @@ let lapMin = 0;
 let lapSec = 0;
 let lapMili = 0;
 
+//start timer after click start button
 function startTimer() {
   if (!isRunning) {
+    //Get Current time
     startTime = Date.now() - timeDifference;
     isRunning = true;
     document.getElementById("lapButton").style.display = "block";
     document.getElementById("playButton").style.display = "none";
     document.getElementById("stopButton").style.display = "block";
     document.getElementById("resetButton").style.display = "block";
+
+    //add setInterval to update Our timer
     timerRef = setInterval(timer, 10);
   }
 }
 
+//This function for time laps after click laps BUtton
 function lapTimer() {
   if (isRunning) {
+    //When laps button created then laps display block
     document.getElementById("laps-timer").style.display = "block";
+
+    //add laps by serially and lap gap and total laps
     let textNode = `
     <div class="laps-row">
     <p class="lap-no">${lapNO < 9 ? "0" + ++lapNO : ++lapNO}</p>
@@ -51,8 +59,11 @@ function lapTimer() {
   }
 }
 
+//StopTimer invoke after click of stop button
 function stopTimer() {
+  //If Timer is Running true then it is stop otherwise  we do nothing
   if (isRunning) {
+    //Calculate timeDiffrece how long our timer Running.
     timeDifference = Date.now() - startTime;
     clearInterval(timerRef);
     isRunning = false;
@@ -61,6 +72,7 @@ function stopTimer() {
   }
 }
 
+//Reset Timer invoke after click Reset Button we reset all the things hide stop ,laps, and reset button and show Start button
 function resetTimer() {
   clearInterval(timerRef);
   isRunning = false;
@@ -86,17 +98,25 @@ function resetTimer() {
   lapSec = 0;
 }
 
+//Timer function update our timer after every 10 milisecond
 function timer() {
+  //Get current Time
   const current = Date.now();
+
+  //Calculate time diffrence between current time and start time
   timeDifference = current - startTime;
 
+  //Calculate hr, min, sec, mili from timeDiffrence
   hr = Math.floor(timeDifference / 3600000);
   min = Math.floor(timeDifference / 60000) % 60;
   sec = Math.floor(timeDifference / 1000) % 60;
   mili = Math.floor((timeDifference % 1000) / 10);
 
-  const time = `${hr < 10 ? "0" + hr : hr}:${min < 10 ? "0" + min : min}:${
+  //store in display variable to time format
+  const display = `${hr < 10 ? "0" + hr : hr}:${min < 10 ? "0" + min : min}:${
     sec < 10 ? "0" + sec : sec
   }:${mili < 10 ? "0" + mili : mili}`;
-  displayTimer.innerText = time;
+
+  //show Timer on Webpages
+  displayTimer.innerText = display;
 }
